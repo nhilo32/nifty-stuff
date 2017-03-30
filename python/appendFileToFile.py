1e9dce48dbdc43efc56a09a16d161ee27655d96f
nhilo32 <nhilo32@yahoo.com>
2017-03-30 12:38:04 -0400

    Update appendFileToFile.py

--- python/appendFileToFile.py
+++ python/appendFileToFile.py
@@ -1,5 +1,11 @@
+#
+# Reads contents of zone.txt and either writes or appends them to the end of ztwo.txt
+# depending on which line you choose. With the if-then-else statement(s) you can also
+# filter content
+#
 with open("zone.txt") as fone:
     with open("ztwo.txt", "a") as ftwo:
+   #with open("ztwo.txt", "w") as ftwo: #if you want to write rather than append use this
         for line in fone:
             #if "ROW" in line: #comment out if you dont need to find anything in file.
                 ftwo.write(line) 
